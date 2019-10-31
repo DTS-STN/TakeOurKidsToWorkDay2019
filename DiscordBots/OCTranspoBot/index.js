0730faec65555e8439e585399044958c43ac71a5
AA <adam.andrews2797@gmail.com>
2019-10-31 16:24:02 -0400

    Innitial commit for OCTranspoBot

--- DiscordBots/OCTranspoBot/index.js
+++ DiscordBots/OCTranspoBot/index.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 
 // API key should be tokenized for security purposes
 const token = process.env.OCTRANSPO_TOKEN || "";
+const appId = "0593fb82";
+const apiKey = "bff5a13a6823320bbe487c96d71483f5";
 
 // Connection event handlers with logging
 client.on("ready", () => {
@@ -17,11 +19,29 @@ client.on("disconnect", () => {
   console.log(`${client.user.tag} is disconnected`);
 });
 
+function getRoutesByStop(stopNumber) {
+  return axios
+    .get(
+      `https://api.octranspo1.com/v1.3/GetRouteSummaryForStop?appID=${appId}&apiKey=${apiKey}&stopNo=${stopNumber}&format=JSON`
+    )
+    .then(response => {
+      console.log(response.data);
+      return response.data;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
 // Message event handler
 client.on("message", msg => {
   // Ignore message from self
-  if (message.author.bot) return;
+  if (msg.author.bot) return;
   if (msg.content.startsWith("!OC")) {
+    getRoutesByStop("3037").then(data => {
+      console.log("API returned: " + data);
+      msg.reply(data.GetRouteSummaryForStopResult.StopDescription);
+    });
   }
 });
 // Create connection to Discord API
